cb16e29340f528dac3802aa53d931df7bd1e6cbf
Ronald Kevin Burton <rkevinburton@charter.net>
2018-09-03 21:07:10 -0500

    Read in Base URL

---
+++ AspReact/Configuration/APISetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspReact.Configuration
+{
+    public class APISetting
+    {
+        public string BaseUrl { get; set; }
+    }
+}
--- AspReact/Controllers/HomeController.cs
+++ AspReact/Controllers/HomeController.cs
@@ -4,11 +4,20 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using AspReact.Configuration;
 
 namespace AspReact.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(IConfiguration configuration)
+        {
+            var settingsSection = configuration.GetSection("API");
+            var settings = settingsSection.Get<APISetting>();
+            Debug.WriteLine(settings.BaseUrl);
+        }
         public IActionResult Index()
         {
             return View();
