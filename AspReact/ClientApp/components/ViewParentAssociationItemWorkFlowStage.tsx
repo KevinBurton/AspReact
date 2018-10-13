6ae4ee2a7803ec0e747922f9202ba7e74f763a48
Kevin Burton <rkevinburton@charter.net>
2018-10-13 08:11:25 -0500

    ViewParentAssociationItemWorkFlowStage format changes

--- AspReact/ClientApp/components/ViewParentAssociationItemWorkFlowStage.tsx
+++ AspReact/ClientApp/components/ViewParentAssociationItemWorkFlowStage.tsx
@@ -9,47 +9,44 @@ type ViewParentAssociationItemWorkFlowStageProps  = ViewParentAssociationItemWor
 
 class ViewParentAssociationItemWorkFlowStage extends React.Component<ViewParentAssociationItemWorkFlowStageProps, any> {
   componentDescriptor: ComponentDescriptor;
-  constructor(props: any) {
-    super(props);
-    this.componentDescriptor = {
-        name: 'TitleDescription',
+    constructor(props: any) {
+      super(props);
+      this.componentDescriptor = {
+          name: 'ViewParentAssociationItemWorkFlowStage',
+          returnObjectType: '',
+          returnObjectIndexed: false,
+          stateFunction:
+          '(objectAssign.default({}, state, { viewParentAssociationItemWorkFlowStage: action.newObject});)',
+          dataDictionary: {
+              ID: 0,
+              ItemId: 0,
+              VendorId: 0
+          }
+      };
+      // componentWillMount
+      this.componentDescriptor = {
+        name: 'ViewParentAssociationItemWorkFlowStage',
         returnObjectType: '',
-        returnObjectIndexed: false,
+        returnObjectIndexed: true,
         stateFunction:
-        '(objectAssign.default({}, state, { TitleDescription: action.newObject});)',
+        '(objectAssign.default({}, state, { viewParentAssociationItemWorkFlowStage: action.newObject});)',
         dataDictionary: {
-            ID: 0,
-            ItemId: 0,
-            VendorId: 0
+            ItemId: ''
         }
-    };
-    // componentWillMount
-    this.componentDescriptor = {
-      name: 'ViewParentAssociationItemWorkFlowStage',
-      returnObjectType: '',
-      returnObjectIndexed: true,
-      stateFunction:
-      '(objectAssign.default({}, state, { SessionFileDetail: action.newObject});)',
-      dataDictionary: {
-          ItemId: ''
-      }
-  }
-
-  this.componentDescriptor.dataDictionary['ItemId'] = this.props.itemId;
-  this.componentDescriptor.dataDictionary['EventTemplateDownload'] = this.props.SessionFileDetail ? this.props.SessionFileDetail.EventTemplateDownload : {};
-  componentData(this.componentDescriptor, 'GetData');
+    }
+    this.componentDescriptor.dataDictionary['ItemId'] = this.props.itemId;
+    this.componentDescriptor.dataDictionary['EventTemplateDownload'] = this.props.SessionFileDetail ? this.props.SessionFileDetail.EventTemplateDownload : {};
+    componentData(this.componentDescriptor, 'GetData');
 
     // Bindings
-}
-componentWillMount() {
-    }
-    render() {
-        var eventTemplateUrl = this.props.SessionFileDetail != null ? this.props.SessionFileDetail.EventTemplateDownload.DefaultValue : '';
-        return (
-            <a href={eventTemplateUrl}
-                id="DownloadTemplate"
-                className="btn btn-primary btn-sm btn-block">Download Template</a>
-        );
+  }
+  render() {
+          var eventTemplateUrl = this.props.SessionFileDetail != null ? this.props.SessionFileDetail.EventTemplateDownload.DefaultValue : '';
+          return (
+              <a href={eventTemplateUrl}
+                  id="DownloadTemplate"
+                  className="btn btn-primary btn-sm btn-block">Download Template</a>
+          );
     }
 }
 
