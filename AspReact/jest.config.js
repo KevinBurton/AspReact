6c9586ebde755e5234af97323347c05ede45d828
Kevin Burton <rkevinburton@charter.net>
2018-10-19 05:13:59 -0500

    move tests

--- AspReact/jest.config.js
+++ AspReact/jest.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+mmodule.exports = {
+  transform: {
+      "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
