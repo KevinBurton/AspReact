4375e48468825b11121301771375265fe366f467
Kevin Burton <rkevinburton@charter.net>
2018-10-21 10:35:00 -0500

    Test setup

--- AspReact/__tests__/CancelButton.test.tsx
+++ AspReact/__tests__/CancelButton.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import CancelButton  from '../ClientApp/components/CancelButton';
 
 describe('A suite', function() {
