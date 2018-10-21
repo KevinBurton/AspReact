38c1c75aced19f785ed95bb7a92f6e88bafa748b
Kevin Burton <rkevinburton@charter.net>
2018-10-21 10:49:29 -0500

    Moved tests

--- AspReact/ClientApp/components/__tests__/CancelButton.test.tsx
+++ AspReact/ClientApp/components/__tests__/CancelButton.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import CancelButton  from '../ClientApp/components/CancelButton';
 
-// https://medium.com/@mateuszsokola/configuring-react-16-jest-enzyme-typescript-7122e1a1e6e8
+// pwdhttps://medium.com/@mateuszsokola/configuring-react-16-jest-enzyme-typescript-7122e1a1e6e8
 describe('A suite', function() {
   it('should render without throwing an error', function() {
     expect(shallow(<CancelButton />).contains(<button className="btn btn-default"></button>)).toBe(true);
