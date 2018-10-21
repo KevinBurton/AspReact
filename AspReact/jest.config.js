7f9e5adada4215942128a627235bb77b730f307b
Kevin Burton <rkevinburton@charter.net>
2018-10-21 09:56:44 -0500

    Test configuration

--- AspReact/jest.config.js
+++ AspReact/jest.config.js
@@ -5,5 +5,8 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?|ts?|js?)$",
   moduleFileExtensions: [...defaults.moduleFileExtensions,"ts", "tsx"],
-  transformIgnorePatterns: ["<rootDir>/node_modules"]
+  transformIgnorePatterns: ["<rootDir>/node_modules"],
+  // Setup Enzyme
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupTestFrameworkScriptFile: "<rootDir>/setupEnzyme.ts"
 };
---
+++ AspReact/setupEnzyme.ts
@@ -0,0 +1,3 @@
+import { configure } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-15';
+configure({ adapter: new EnzymeAdapter() });
